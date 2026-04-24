5a051179881c1e77dd38ecc68473e26cfebab238
mars050504 <153522723+mars050504@users.noreply.github.com>
2026-04-24 20:43:26 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
 
     <title>Register</title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <link rel="stylesheet" href="/css/login.css">
 
