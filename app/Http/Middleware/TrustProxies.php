c167c0845b19f6abf196e7ad3d5ca21b9671cfa4
mars050504 <153522723+mars050504@users.noreply.github.com>
2026-04-25 15:24:34 +0700

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // 🔥 WAJIB untuk Railway
 
     /**
      * The headers that should be used to detect proxies.
