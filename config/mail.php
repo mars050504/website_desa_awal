00df194028891c7b4d5ed145e8b02224b6a6f020
mars050504 <153522723+mars050504@users.noreply.github.com>
2026-05-03 00:01:46 +0700

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => env('MAIL_TIMEOUT', 60),
             'auth_mode' => null,
         ],
 
