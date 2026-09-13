894e70181f6212937209adf46dd4e98385d24cea
mars050504 <153522723+mars050504@users.noreply.github.com>
2026-09-13 13:56:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -48,11 +48,7 @@ Route::get('/register', [AuthController::class, 'register']);
 Route::post('/register', [AuthController::class, 'store']);
 
 Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/verify-otp', function () {
-    return view('auth.verify-otp');
-});
 
-Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
 
 ////////////////////////////////////////////////////
 /// ✉️ EMAIL VERIFICATION
